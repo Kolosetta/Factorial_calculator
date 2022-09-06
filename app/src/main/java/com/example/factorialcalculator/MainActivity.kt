c41bb68f1b535e1c18fff5926446cbda27d49643
tr00per <kolosovskiy91@gmail.com>
2022-09-06 23:38:22 +0300

    Вычисления переведены в фоновый поток

--- app/src/main/java/com/example/factorialcalculator/MainActivity.kt
+++ app/src/main/java/com/example/factorialcalculator/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.factorialcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
--- app/src/main/java/com/example/factorialcalculator/MainViewModel.kt
+++ app/src/main/java/com/example/factorialcalculator/MainViewModel.kt
@@ -4,8 +4,10 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.math.BigInteger
 
 class MainViewModel : ViewModel() {
 
@@ -13,17 +15,27 @@ class MainViewModel : ViewModel() {
     val state: LiveData<State>
         get() = _state
 
-    fun calculate(value: String?){
+    fun calculate(value: String?) {
         _state.value = Progress
-        if(value.isNullOrBlank()){
+        if (value.isNullOrBlank()) {
             _state.value = Error
             return
         }
-        viewModelScope.launch {
+        viewModelScope.launch() {
             val number = value.toLong()
             //calculating
-            delay(1000)
-            _state.value = Factorial(number.toString())
+            val result = factorial(number)
+            _state.value = Factorial(result)
+        }
+    }
+
+    private suspend fun factorial(number: Long): String {
+        return withContext(Dispatchers.Default){
+            var result = BigInteger.ONE
+            for (i in 1..number) {
+                result = result.multiply(BigInteger.valueOf(i))
+            }
+            result.toString()
         }
     }
 
